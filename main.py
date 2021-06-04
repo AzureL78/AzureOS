d9463fdd9c85ea33b06f3f9ace18e2ec1bd54ea1
azure64 <lukeweilke80@gmail.com>
2021-06-04 19:35:05 -0400

    Added uname command, based on coreutils

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
 from colored import fg, bg, attr
+import time
+
+hostname = "qamu-system-azure"
+
+user = "root"
+password = "root"
+
+print("AzureOS on x86_64-qamu-system\nLoading kernel.img...")
+time.sleep(3)
+
+def uname():
+    print(hostname)
+
+def bash():
+    
